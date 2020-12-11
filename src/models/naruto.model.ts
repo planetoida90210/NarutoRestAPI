b00dbe16f9bd73cefcf13d7b53f3d713670a43ff
planetoida90210 <galeriakoloru90210@gmail.com>
2020-12-11 18:56:34 +0100

    add additional queries to schema

--- src/models/naruto.model.ts
+++ src/models/naruto.model.ts
@@ -5,12 +5,18 @@ const NarutoSchema = new mongoose.Schema({
   surname: String,
   country: String,
   hiddenVilage: String,
+  clan: String,
   rank: String,
   age: Number,
+  notableFeatures: String,
+  currentStatus: String,
   personalData: {
     birthday: String,
     height: Number,
     weight: Number,
     bloodType: String,
-  }
+  },
+ image: String,
 })
+
+export const Naruto = mongoose.model("Naruto", NarutoSchema)
--- src/services/naruto.service.ts
+++ src/services/naruto.service.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express';
 import {WELCOME_MESSAGE} from '../constants/narutoApi.constants';
+import {Naruto} from '../models/naruto.model'
 
 export class NarutoService {
   public welcomeMessage(req: Request, res: Response){
